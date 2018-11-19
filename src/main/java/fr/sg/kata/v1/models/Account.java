8f0b02715ae9adeeed0b1bdd84b046747886e87d
riadm <riad.moussaoui@hotmail.com>
2018-11-19 22:47:22 +0100

    Ajout de lombok

--- src/main/java/fr/sg/kata/v1/models/Account.java
+++ src/main/java/fr/sg/kata/v1/models/Account.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Account {
 	@Id
@@ -14,59 +17,4 @@ public class Account {
 	@ManyToOne
 	private Client owner;
 	
-	public String getAccountId() {
-		return accountId;
-	}
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
-	}
-	public BigDecimal getBalance() {
-		return balance;
-	}
-	public void setBalance(BigDecimal balance) {
-		this.balance = balance;
-	}
-	public Client getOwner() {
-		return owner;
-	}
-	public void setOwner(Client owner) {
-		this.owner = owner;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((accountId == null) ? 0 : accountId.hashCode());
-		result = prime * result + ((balance == null) ? 0 : balance.hashCode());
-		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Account other = (Account) obj;
-		if (accountId == null) {
-			if (other.accountId != null)
-				return false;
-		} else if (!accountId.equals(other.accountId))
-			return false;
-		if (balance == null) {
-			if (other.balance != null)
-				return false;
-		} else if (!balance.equals(other.balance))
-			return false;
-		if (owner == null) {
-			if (other.owner != null)
-				return false;
-		} else if (!owner.equals(other.owner))
-			return false;
-		return true;
-	}
 }
--- src/main/java/fr/sg/kata/v1/models/Client.java
+++ src/main/java/fr/sg/kata/v1/models/Client.java
@@ -3,6 +3,9 @@ package fr.sg.kata.v1.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Client {
 	
@@ -12,36 +15,4 @@ public class Client {
 	private String password;
 	private String firstname;
 	private String lastname;
-	
-	public String getClientId() {
-		return clientId;
-	}
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String userName) {
-		this.username = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	
 }
