16da538acf0982f6a91419cc11a0be2b0af1f038
riadm <riad.moussaoui@hotmail.com>
2018-11-27 12:30:55 +0100

    Fixed integration tests with jwt authentification

--- src/test/java/fr/sg/kata/v1/controllers/ControllerIntegrationTest.java
+++ src/test/java/fr/sg/kata/v1/controllers/ControllerIntegrationTest.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,14 +17,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import fr.sg.kata.v1.data.TransactionData;
 import fr.sg.kata.v1.data.TransactionRequestData;
@@ -30,7 +38,10 @@ import fr.sg.kata.v1.models.TransactionType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@Configuration
+//@Ignore
 public class ControllerIntegrationTest {
+	private static final String ACCOUNT_ID = "1111";
 	
 	@LocalServerPort
     private int serverPort;
@@ -40,29 +51,54 @@ public class ControllerIntegrationTest {
 	
 	private String url;
 	
+	private String jwtToken;
+	
 	@Before
-	public void setup() {
+	public void setup() throws JSONException {
 		url = "http://localhost:" + serverPort + "/account-manager/v1/accounts/{accountId}/transactions";
+		jwtToken = createJwtToken();
 	}
 	
+	private String createJwtToken() throws JSONException {
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+
+		map.add("grant_type", "password");
+		map.add("username", "john.doe");
+		map.add("password", "jwtpass");
+		
+		ResponseEntity<String> res = restTemplate.withBasicAuth("testjwtclientid", "XY7kmzoNzl100").postForEntity("http://localhost:" + serverPort + "/oauth/token", map, String.class);
+		return new JSONObject(res.getBody()).getString("access_token");
+	}
+	
+	
 	@Test
-	@WithMockUser(username = "john.doe", authorities = { "CLIENT" })
+	@WithMockUser(username = "john.doe", roles={ "CLIENT" })
 	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "/cleantransactions.sql")
-	public void testDoTransaction() {
-		
-		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
-		
+	public void testDoTransaction() throws JSONException {
+
 		TransactionRequestData transactionRequestData = new TransactionRequestData();
 		transactionRequestData.setAmount(new BigDecimal("500"));
 		transactionRequestData.setTransactionType(TransactionType.D);
-		ResponseEntity<TransactionData> response = restTemplate.postForEntity(url, transactionRequestData, TransactionData.class, map);
+		
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<TransactionRequestData> entity = new HttpEntity<>(transactionRequestData, requestHeaders);
+
+		Map<String, String> map = new HashMap<String, String>();
+        map.put("accountId", ACCOUNT_ID);
+
+		ResponseEntity<TransactionData> response = restTemplate.postForEntity(url, entity, TransactionData.class, map);
 		
 		assertTrue(response.getStatusCode().equals(HttpStatus.CREATED));
 		HttpHeaders headers = response.getHeaders();
 		TransactionData createdTransaction = response.getBody();
 		
-		assertTrue("1111".equals(createdTransaction.getAccountId()));
+		assertTrue(ACCOUNT_ID.equals(createdTransaction.getAccountId()));
 		assertTrue(transactionRequestData.getAmount().equals(createdTransaction.getAmount()));
 		assertTrue(transactionRequestData.getTransactionType().equals(createdTransaction.getTransactionType()));
 		assertTrue(headers.containsKey(HttpHeaders.LOCATION));
@@ -75,11 +111,16 @@ public class ControllerIntegrationTest {
 	})
 	public void testAccountHistoryWithDateRange() {
 		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
+        map.put("accountId", ACCOUNT_ID);
         url = url.concat("?start=09-07-2018&end=11-08-2018");
 
-		ResponseEntity<Collection> history = restTemplate.getForEntity(url, Collection.class, map);
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<Object> entity = new HttpEntity<>(requestHeaders);
 		
+		ResponseEntity<Collection> history = restTemplate.exchange(url, HttpMethod.GET, entity, Collection.class, map);
+
 		assertTrue(history.getBody().size() == 2);
 		
 	}
@@ -91,11 +132,16 @@ public class ControllerIntegrationTest {
 	})
 	public void testAccountHistoryWithStartDate() {
 		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
+        map.put("accountId", ACCOUNT_ID);
         url = url.concat("?start=09-06-2018");
 
-		ResponseEntity<Collection> history = restTemplate.getForEntity(url, Collection.class, map);
-		
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<Object> entity = new HttpEntity<>(requestHeaders);
+
+		ResponseEntity<Collection> history = restTemplate.exchange(url, HttpMethod.GET, entity, Collection.class, map);
+
 		assertTrue(history.getBody().size() == 6);
 		
 	}
@@ -107,10 +153,15 @@ public class ControllerIntegrationTest {
 	})
 	public void testAccountHistoryWithEndDate() {
 		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
+        map.put("accountId", ACCOUNT_ID);
         url = url.concat("?end=09-08-2018");
 
-		ResponseEntity<Collection> history = restTemplate.getForEntity(url, Collection.class, map);
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<Object> entity = new HttpEntity<>(requestHeaders);
+
+		ResponseEntity<Collection> history = restTemplate.exchange(url, HttpMethod.GET, entity, Collection.class, map);
 		
 		assertTrue(history.getBody().size() == 4);
 		
@@ -123,9 +174,14 @@ public class ControllerIntegrationTest {
 	})
 	public void testAccountHistoryWithoutDateRange() {
 		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
+        map.put("accountId", ACCOUNT_ID);
 
-		ResponseEntity<Collection> history = restTemplate.getForEntity(url, Collection.class, map);
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<Object> entity = new HttpEntity<>(requestHeaders);
+
+		ResponseEntity<Collection> history = restTemplate.exchange(url, HttpMethod.GET, entity, Collection.class, map);
 		
 		assertTrue(history.getBody().size() == 8);
 		
@@ -139,12 +195,18 @@ public class ControllerIntegrationTest {
 	})
 	public void testShouldRaiseException() {
 		Map<String, String> map = new HashMap<String, String>();
-        map.put("accountId", "1111");
+        map.put("accountId", ACCOUNT_ID);
 		
 		TransactionRequestData transactionRequestData = new TransactionRequestData();
 		transactionRequestData.setAmount(null);
 		transactionRequestData.setTransactionType(TransactionType.D);
-		ResponseEntity<Object> response = restTemplate.postForEntity(url, transactionRequestData, Object.class, map);
+		
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+		requestHeaders.set("Authorization", "Bearer " + jwtToken);
+		HttpEntity<TransactionRequestData> entity = new HttpEntity<>(transactionRequestData, requestHeaders);
+
+		ResponseEntity<Object> response = restTemplate.postForEntity(url, entity, Object.class, map);
 		
 		assertTrue(response.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY));
 		
